70925e819f7fff9a2aec481936700f66ad4894c4
Siddarath Kumar <xdx4418@autuni.ac.nz>
2021-05-10 20:44:14 +1200

    cleaned up the GuestChooseHotel class and formatted the hotel rooms menu

--- Project2HotelApp/src/project2hotelapp/GuestChooseHotel.java
+++ Project2HotelApp/src/project2hotelapp/GuestChooseHotel.java
@@ -25,7 +25,7 @@ import javax.swing.JRadioButton;
  *
  * @author Siddarath
  */
-public class GuestChooseHotel extends JFrame {
+public class GuestChooseHotel extends JFrame implements ActionListener{
     
     private static GuestChooseRoom bookRooms;
     private JLabel title, instruction;
@@ -59,31 +59,6 @@ public class GuestChooseHotel extends JFrame {
         menuPanel.setPreferredSize(new Dimension(600,450));
         menuPanel.add(instruction);
         group = new ButtonGroup();
-        
-        //String hello = "hi"
-        /*addHotelLocationBtn(String.format("%-40s", "Title: " +locationsMenu.get(0).getTitle())+ String.format("%-30s", "Location: " +locationsMenu.get(0).getLocationType())+
-                String.format("%-20s", "Rating: " +locationsMenu.get(0).getRatingType()) + String.format("%-20s", "Vacancy: " +locationsMenu.get(0).getVacancyType()));
-        
-        addHotelLocationBtn(String.format("%-40s", "Title: " +locationsMenu.get(1).getTitle())+ String.format("%-30s", "Location: " +locationsMenu.get(1).getLocationType())+
-                String.format("%-20s", "Rating: " +locationsMenu.get(1).getRatingType()) + String.format("%-20s", "Vacancy: " +locationsMenu.get(1).getVacancyType()));
-        
-        addHotelLocationBtn(String.format("%-40s", "Title: " +locationsMenu.get(2).getTitle())+ String.format("%-30s", "Location: " +locationsMenu.get(2).getLocationType())+
-                String.format("%-20s", "Rating: " +locationsMenu.get(2).getRatingType()) + String.format("%-20s", "Vacancy: " +locationsMenu.get(2).getVacancyType()));
-        
-        addHotelLocationBtn(String.format("%-40s", "Title: " +locationsMenu.get(3).getTitle())+ String.format("%-30s", "Location: " +locationsMenu.get(3).getLocationType())+
-                String.format("%-20s", "Rating: " +locationsMenu.get(3).getRatingType()) + String.format("%-20s", "Vacancy: " +locationsMenu.get(3).getVacancyType()));
-        
-        addHotelLocationBtn(String.format("%-40s", "Title: " +locationsMenu.get(4).getTitle())+ String.format("%-30s", "Location: " +locationsMenu.get(4).getLocationType())+
-                String.format("%-20s", "Rating: " +locationsMenu.get(4).getRatingType()) + String.format("%-20s", "Vacancy: " +locationsMenu.get(4).getVacancyType()));
-        
-        addHotelLocationBtn(String.format("%-40s", "Title: " +locationsMenu.get(5).getTitle())+ String.format("%-30s", "Location: " +locationsMenu.get(5).getLocationType())+
-                String.format("%-20s", "Rating: " +locationsMenu.get(5).getRatingType()) + String.format("%-20s", "Vacancy: " +locationsMenu.get(5).getVacancyType()));
-        
-        addHotelLocationBtn(String.format("%-40s", "Title: " +locationsMenu.get(6).getTitle())+ String.format("%-30s", "Location: " +locationsMenu.get(6).getLocationType())+
-                String.format("%-20s", "Rating: " +locationsMenu.get(6).getRatingType()) + String.format("%-20s", "Vacancy: " +locationsMenu.get(6).getVacancyType()));
-        
-        addHotelLocationBtn(String.format("%-40s", "Title: " +locationsMenu.get(7).getTitle())+ String.format("%-30s", "Location: " +locationsMenu.get(7).getLocationType())+
-                String.format("%-20s", "Rating: " +locationsMenu.get(7).getRatingType()) + String.format("%-20s", "Vacancy: " +locationsMenu.get(7).getVacancyType()));*/
         
         locationButtons [0] = new JRadioButton("Title: Auckland Hotel - 31 Starcent Ave                          Location: Auckland                       Rating: Five      Vacancy: Yes");
         locationButtons [1] = new JRadioButton("Title: Rotorua Hotel - 17 Geyser Crescent                      Location: Rotorua                        Rating: Three     Vacancy: Yes");
@@ -125,31 +100,7 @@ public class GuestChooseHotel extends JFrame {
         nextButton = new JButton("Next");
         nextButton.setPreferredSize(new Dimension(200,70));
         nextButton.setFont(new Font("Arial", Font.BOLD, 24));
-        nextButton.addActionListener(new ActionListener()
-        {
-           @Override
-           public void actionPerformed(ActionEvent e)
-           {
-               if(locationButtons[0].isSelected() || locationButtons[1].isSelected() || locationButtons[3].isSelected() || 
-                       locationButtons[5].isSelected() || locationButtons[7].isSelected())
-               {
-                   int userConfirmation = JOptionPane.showConfirmDialog(new JFrame(), "Would you like to continue?");
-                   
-                   if(userConfirmation == JOptionPane.YES_OPTION)
-                   {
-                       dispose();
-                       bookRooms = new GuestChooseRoom();
-                       //add code here to add to cart or table etc
-                   }
-               }
-               else
-               {
-                   JOptionPane.showMessageDialog(new JFrame(), "Please select a hotel location before moving to the next stage", "Invalid Selection",
-                    JOptionPane.ERROR_MESSAGE);
-               }
-               
-           }
-        });
+        nextButton.addActionListener(this);
         
         bottomPanel.add(backButton);
         bottomPanel.add(nextButton);
@@ -162,35 +113,25 @@ public class GuestChooseHotel extends JFrame {
         this.setVisible(true);
     }
    
-    /*public void addHotelLocationBtn(String name)
+    public void actionPerformed(ActionEvent e)
     {
-        addHotelLocationBtn("Title: Auckland Hotel - 31 Starcent Ave                          Location: Auckland                       Rating: Five      Vacancy: Yes");
-        addHotelLocationBtn("Title: Rotorua Hotel - 17 Geyser Crescent                      Location: Rotorua                        Rating: Three     Vacancy: Yes");
-        addHotelLocationBtn("Title: Taupo Hotel - 48 Lake Ave                                       Location: Taupo                           Rating: Four       Vacancy: No");
-        addHotelLocationBtn("Title: Palmerston Hotel - 69 Leggy Ave                            Location: Palmerston Hotel        Rating: Four       Vacancy: Yes");
-        addHotelLocationBtn("Title: Wellington Hotel - 128 Bee Ave                               Location: Wellington                   Rating: Five        Vacancy: No");
-        addHotelLocationBtn("Title: Christchurch Hotel - 27 Meadow Lane                    Location: Christchurch               Rating: Four        Vacancy: Yes");
-        addHotelLocationBtn("Title: Dunedin Hotel - 987 Lonely Road                            Location: Dunedin                        Rating: Four        Vacancy: No");
-        addHotelLocationBtn("Title: Queenstown Hotel - 52 New South Drive               Location: Queenstown                Rating: Five         Vacancy: Yes");
-    
-        JRadioButton locationButton = new JRadioButton(name);
-        locationButton.setPreferredSize(new Dimension(1100,50));
-        locationButton.setFont(new Font("Arial", Font.BOLD, 18));
-        group.add(locationButton);
-        menuPanel.add(locationButton);
-        
-        ActionListener listener = new ActionListener()
+        if(locationButtons[0].isSelected() || locationButtons[1].isSelected() || locationButtons[3].isSelected() || 
+                locationButtons[5].isSelected() || locationButtons[7].isSelected())
         {
-            public void actionPerformed(ActionEvent event)
+            int userConfirmation = JOptionPane.showConfirmDialog(new JFrame(), "Would you like to continue?");
+
+            if(userConfirmation == JOptionPane.YES_OPTION)
             {
-                System.out.println("Your chose" +event);
-                
-                if (locaton)
-                {
-                    System.out.println("invalid choice");
-                }
+                dispose();
+                bookRooms = new GuestChooseRoom();
+                //add code here to add to cart or table etc
             }
-        };
-        
-    }*/
+        }
+        else
+        {
+            JOptionPane.showMessageDialog(new JFrame(), "Please select a hotel location before moving to the next stage", "Invalid Selection",
+             JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
 }
--- Project2HotelApp/src/project2hotelapp/GuestChooseRoom.java
+++ Project2HotelApp/src/project2hotelapp/GuestChooseRoom.java
@@ -9,11 +9,16 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
@@ -22,27 +27,23 @@ import javax.swing.border.EmptyBorder;
  *
  * @author Siddarath
  */
-public class GuestChooseRoom extends JFrame{
+public class GuestChooseRoom extends JFrame implements ActionListener{
     
     private JLabel title, instruction, standardTitle, deluxeTitle, twinTitle, queenTitle, familyTitle, couplesTitle;
     private JPanel headerPanel, menuPanel, bottomPanel;
     private JComboBox<Integer> standardRoomBox, deluxeRoomBox, twinRoomBox, queenRoomBox, familyRoomBox, couplesRoomBox;
+    private JButton backButton, nextButton;
     
     Integer[] roomQuantity = {0,1,2,3,4,5};
     
     
     public GuestChooseRoom()
     {
-        /*setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100,100,450,300);
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5,5,5,5));
-        contentPane.setLayout(new BorderLayout(0,0));*/
         ArrayList<RoomMenu> roomMenu = new ArrayList<RoomMenu>();
         roomMenu = RoomMenu.showMenu();
-        Border blackline = BorderFactory.createLineBorder(Color.BLACK, 1); 
+        Border blackline = BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.BLACK, 2), BorderFactory.createLineBorder(Color.LIGHT_GRAY, 10));
         
-        title = new JLabel("Hotel Locations Menu\n");
+        title = new JLabel("Rooms Menu\n");
         title.setFont(new Font("Arial", Font.BOLD, 30));
         title.setForeground(Color.YELLOW);
         
@@ -90,32 +91,54 @@ public class GuestChooseRoom extends JFrame{
         standardTitle.setBorder(blackline);
         deluxeTitle = new JLabel("Title: " +roomMenu.get(1).getTitle()+ "                     Room Type: " +roomMenu.get(1).getRoomType()+ "                     Price: $" +roomMenu.get(1).getPrice()+ "             Rooms: ");
         deluxeTitle.setFont(new Font("Arial", Font.BOLD, 20));
+        deluxeTitle.setBorder(blackline);
         twinTitle = new JLabel("Title: " +roomMenu.get(2).getTitle()+ "                       Room Type: " +roomMenu.get(2).getRoomType()+ "                    Price: $" +roomMenu.get(2).getPrice()+ "                  Rooms: ");
         twinTitle.setFont(new Font("Arial", Font.BOLD, 20));
+        twinTitle.setBorder(blackline);
         queenTitle = new JLabel("Title: " +roomMenu.get(3).getTitle()+ "                        Room Type: " +roomMenu.get(3).getRoomType()+ "                Price: $" +roomMenu.get(3).getPrice()+ "                Rooms: ");
         queenTitle.setFont(new Font("Arial", Font.BOLD, 20)); 
+        queenTitle.setBorder(blackline);
         familyTitle = new JLabel("Title: " +roomMenu.get(4).getTitle()+ "                             Room Type: " +roomMenu.get(4).getRoomType()+ "      Price: $" +roomMenu.get(4).getPrice()+ "                            Rooms: ");
         familyTitle.setFont(new Font("Arial", Font.BOLD, 20));
+        familyTitle.setBorder(blackline);
         couplesTitle = new JLabel("Title: " +roomMenu.get(5).getTitle()+ "       Room Type: " +roomMenu.get(5).getRoomType()+ "     Price: $" +roomMenu.get(5).getPrice()+ "         Rooms: ");
         couplesTitle.setFont(new Font("Arial", Font.BOLD, 20));
+        couplesTitle.setBorder(blackline);
         
-        
+        menuPanel.add(Box.createVerticalStrut(10));
         menuPanel.add(standardTitle);
         menuPanel.add(standardRoomBox);
+        menuPanel.add(Box.createVerticalStrut(75));
         menuPanel.add(deluxeTitle);
         menuPanel.add(deluxeRoomBox);
+        menuPanel.add(Box.createVerticalStrut(75));
         menuPanel.add(twinTitle);
         menuPanel.add(twinRoomBox);
+        menuPanel.add(Box.createVerticalStrut(75));
         menuPanel.add(queenTitle);
         menuPanel.add(queenRoomBox);
+        menuPanel.add(Box.createVerticalStrut(75));
         menuPanel.add(familyTitle);
         menuPanel.add(familyRoomBox);
+        menuPanel.add(Box.createVerticalStrut(75));
         menuPanel.add(couplesTitle);
         menuPanel.add(couplesRoomBox);
         
         this.add(menuPanel, BorderLayout.CENTER);
         
         bottomPanel = new JPanel();
+        backButton = new JButton("Go Back");
+        backButton.setPreferredSize(new Dimension(200,70));
+        backButton.setFont(new Font("Arial", Font.BOLD, 24));
+        
+        nextButton = new JButton("Next");
+        nextButton.setPreferredSize(new Dimension(200,70));
+        nextButton.setFont(new Font("Arial", Font.BOLD, 24));
+        nextButton.addActionListener(this);
+        
+        
+        bottomPanel.add(backButton);
+        bottomPanel.add(nextButton);
         this.add(bottomPanel, BorderLayout.SOUTH);
         
         this.setTitle("Guests booking rooms");
@@ -124,5 +147,28 @@ public class GuestChooseRoom extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
+    
+    public void actionPerformed(ActionEvent e)
+           {
+               if(standardRoomBox.getSelectedItem().equals(0) && deluxeRoomBox.getSelectedItem().equals(0) && twinRoomBox.getSelectedItem().equals(0)
+                       && queenRoomBox.getSelectedItem().equals(0) && couplesRoomBox.getSelectedItem().equals(0))
+               {
+                   JOptionPane.showMessageDialog(new JFrame(), "Please select the rooms you would like to book before moving to the next stage", "Invalid Selection",
+                    JOptionPane.ERROR_MESSAGE);
+               }
+               else
+               {
+                   int userConfirmation = JOptionPane.showConfirmDialog(new JFrame(), "Would you like to continue?");
+                   
+                   if(userConfirmation == JOptionPane.YES_OPTION)
+                   {
+                       dispose();
+                       
+                       //add code here to add to cart or table etc
+                   }
+ 
+               }
+               
+           }
     
 }
