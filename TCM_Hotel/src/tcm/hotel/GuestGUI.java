d69f58e76a0a0c4420d2b1d2ff2b26b007a8880d
Anuk Silva <vwv9667@autuni.ac.nz>
2021-05-10 20:38:18 +1200

    Modified GuestGUI Format to Consist of 3 Panels

--- TCM_Hotel/src/tcm/hotel/GuestGUI.java
+++ TCM_Hotel/src/tcm/hotel/GuestGUI.java
@@ -12,6 +12,8 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -78,16 +80,149 @@ public class GuestGUI extends JFrame implements ActionListener{
     JButton confirmDetails;
     JButton resetDetails;
     
+    JPanel centrePanel;
+    
     private GuestForm guestForm;
     
     public GuestGUI(){
         guestForm = new GuestForm();
         formFrame = new JFrame();
-        formFrame.setBounds(800,200,600, 750);
+        formFrame.setBounds(800,200,600,900);
         formFrame.setTitle("Guest Form");
         formFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         headerPanel = new JPanel();
+        headerPanel.add(Box.createVerticalStrut(100));
+        headerPanel.setBackground(Color.blue);
+        welcomeLabel = new JLabel("Personal Details");
+        welcomeLabel.setForeground(Color.white);
+        welcomeLabel.setFont(new Font("Arial", Font.BOLD, 30));
+        headerPanel.add(welcomeLabel);
+        
+        centrePanel = new JPanel(null);
+        
+        fName = new JLabel("First Name: ");
+        fName.setBounds(200, 25, 150, 30);
+        fName.setFont(new Font("Arial", Font.BOLD, 17));
+        fNameField = new JTextField("", 30);
+        fNameField.setBounds(300, 25, 200, 30);
+        
+        lName = new JLabel("Last Name: ");
+        lName.setBounds(200, 85, 150, 30);
+        lName.setFont(new Font("Arial", Font.BOLD, 17));
+        lNameField = new JTextField("", 30);
+        lNameField.setBounds(300, 85, 200, 30);
+        
+        age = new JLabel("Age: ");
+        age.setBounds(250, 145, 300, 30);
+        age.setFont(new Font("Arial", Font.BOLD, 17));
+        ageField = new JTextField("", 20);
+        ageField.setBounds(300, 145, 100, 30);
+        
+        phoneNumber = new JLabel("Contact Phone: ");
+        phoneNumber.setBounds(160, 205, 300, 30);
+        phoneNumber.setFont(new Font("Arial", Font.BOLD, 17));
+        phoneNumField = new JTextField("", 30);
+        phoneNumField.setBounds(300, 205, 100, 30);
+        
+        emailAddress = new JLabel("Contact Email: ");
+        emailAddress.setBounds(170, 265, 300, 30);
+        emailAddress.setFont(new Font("Arial", Font.BOLD, 17));
+        emailField = new JTextField("", 30);
+        emailField.setBounds(300, 265, 200, 30);
+        
+        paymentHeader = new JPanel();
+        paymentHeader.add(Box.createVerticalStrut(100));
+        paymentHeader.setBackground(Color.blue);
+        paymentLabel = new JLabel("Payment Details");
+        paymentLabel.setForeground(Color.white);
+        paymentLabel.setFont(new Font("Arial", Font.BOLD, 30));
+        paymentHeader.add(paymentLabel);
+        paymentHeader.setBounds(0, 355, 600, 100);
+        
+        accountNumber = new JLabel("Bank Account Number: ");
+        accountNumber.setBounds(100, 475, 200, 100);
+        accountNumber.setFont(new Font("Arial", Font.BOLD, 17));
+        accNumField = new JTextField("", 30);
+        accNumField.setBounds(300, 515, 200, 30);
+
+        accountPin = new JLabel("Bank Account Pin: ");
+        accountPin.setBounds(100, 535, 200, 100);
+        accountPin.setFont(new Font("Arial", Font.BOLD, 17));
+        accPinField = new JPasswordField("", 30);
+        accPinField.setBounds(300, 565, 200, 30);
+        
+        centrePanel.add(fName);
+        centrePanel.add(fNameField);
+        centrePanel.add(lName);
+        centrePanel.add(lNameField);
+        centrePanel.add(age);
+        centrePanel.add(ageField);
+        centrePanel.add(phoneNumber);
+        centrePanel.add(phoneNumField);
+        centrePanel.add(emailAddress);
+        centrePanel.add(emailField);
+        centrePanel.add(paymentHeader);
+        centrePanel.add(accountNumber);
+        centrePanel.add(accNumField);
+        centrePanel.add(accountPin);
+        centrePanel.add(accPinField);
+        
+        buttonPanel = new JPanel();
+        buttonPanel.setBackground(Color.blue);
+        buttonPanel.setBounds(0, 275, 600, 700);
+        
+        confirmDetails = new JButton("Confirm Details " + "\u2192");
+        confirmDetails.setPreferredSize(new Dimension(170, 70));
+        confirmDetails.setFont(new Font("Arial", Font.BOLD, 17));
+        confirmDetails.setBorder(new LineBorder(Color.black, 4));
+        confirmDetails.setForeground(Color.black);
+        confirmDetails.addActionListener(this);
+        
+        Color DefaultColour = confirmDetails.getBackground();
+        
+        confirmDetails.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseEntered(MouseEvent arg0){
+                confirmDetails.setBackground(Color.LIGHT_GRAY);
+            }
+            
+            @Override
+            public void mouseExited(MouseEvent arg0){
+                confirmDetails.setBackground(DefaultColour);
+        }
+        
+        });
+        
+        resetDetails = new JButton("Reset " + "\u2190");
+        resetDetails.setPreferredSize(new Dimension(170, 70));
+        resetDetails.setFont(new Font("Arial", Font.BOLD, 17));
+        resetDetails.setBorder(new LineBorder(Color.black, 4));
+        resetDetails.setForeground(Color.black);
+        resetDetails.addActionListener(this);
+        
+        resetDetails.addMouseListener(new MouseAdapter(){
+            @Override
+            public void mouseEntered(MouseEvent arg0){
+                resetDetails.setBackground(Color.LIGHT_GRAY);
+            }
+            
+            @Override
+            public void mouseExited(MouseEvent arg0){
+                resetDetails.setBackground(DefaultColour);
+        }
+        
+        });
+        
+        buttonPanel.add(confirmDetails);
+        buttonPanel.add(resetDetails);
+        
+        formFrame.add(headerPanel, BorderLayout.NORTH);
+        formFrame.add(centrePanel, BorderLayout.CENTER);
+        formFrame.add(buttonPanel, BorderLayout.SOUTH);
+        formFrame.setVisible(true);
+        
+        /*headerPanel = new JPanel();
         headerPanel.add(Box.createVerticalStrut(50));
         
         formPanel = new JPanel();
@@ -230,8 +365,8 @@ public class GuestGUI extends JFrame implements ActionListener{
 
         formFrame.add(headerPanel, BorderLayout.NORTH);
         formFrame.add(formPanel, BorderLayout.CENTER);
-        formFrame.add(buttonPanel, BorderLayout.SOUTH);
-        formFrame.setVisible(true);
+        formFrame.add(buttonPanel, BorderLayout.SOUTH);*/
+        
     }
     
     public void actionPerformed(ActionEvent e){
