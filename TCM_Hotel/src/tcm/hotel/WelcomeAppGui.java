97617992150d56cc90ff443bb2395dc13c01e399
Faaris Ali Khan <tdv2188@autuni.ac.nz>
2021-05-16 17:23:31 +1200

    Making steady changes, added button, panels, etc

--- TCM_Hotel/src/tcm/hotel/WelcomeAppGui.java
+++ TCM_Hotel/src/tcm/hotel/WelcomeAppGui.java
@@ -8,59 +8,106 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-
-//Add A comment
+import javax.swing.border.LineBorder;
 
 /**
  *
- * @author faari
+ * @author faaris
  */
 
-public class WelcomeAppGui extends JFrame //Making a change for testing purposes
+public class WelcomeAppGui extends JFrame implements ActionListener //Making a change for testing purposes
 {
-    JPanel topPanel;
-    JPanel middlePanel;
-    JPanel bottomPanel;
+    private JFrame welcomeFrame;
     
-    JLabel welcomeLabel;
-    JLabel subWelcomeLabel;
+    private JPanel topPanel;
+    private JPanel middlePanel;
+    private JPanel bottomPanel;
     
-    JButton buttonUpdate;
+    private JLabel welcomeLabel;
+    private JLabel subWelcomeLabel;
+    
+    private JButton buttonOne;
+    private JButton buttonTwo;
+    private JButton buttonThree;
+    private JButton buttonFour;
     
     public WelcomeAppGui()
     {
+        //Create the Frame
+        welcomeFrame = new JFrame("TCM Hotel Online Booking System");
+        welcomeFrame.setBounds(500, 500, 1200, 900);
+        welcomeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         //Initialize the Panels
         topPanel = new JPanel();
-        topPanel.setPreferredSize(new Dimension(500, 500));
+        topPanel.add(Box.createVerticalStrut(120));
+        topPanel.setBackground(Color.blue);
         
         middlePanel = new JPanel();
-        bottomPanel = new JPanel();
-            
+        middlePanel.add(Box.createVerticalStrut(120));
+        //middlePanel.setBackground(Color.GRAY);
+
+        //Create labels
         welcomeLabel = new JLabel("Welcome to the TCM Online Booking System!");
+        welcomeLabel.setForeground(Color.white);
         welcomeLabel.setFont(new Font("Arial", Font.BOLD, 30));
+        welcomeLabel.setBounds(450, 450, 300, 300);
         
+        subWelcomeLabel = new JLabel("Please choose one of the options below to continue through the system...");
+        subWelcomeLabel.setForeground(Color.white);
+        subWelcomeLabel.setFont(new Font("Arial", Font.BOLD, 15));
+        subWelcomeLabel.setBounds(500, 500, 900, 950);
         
-        subWelcomeLabel = new JLabel("Select one of the options below to proceed...");
-        subWelcomeLabel.setFont(new Font("Times New Roman", Font.ITALIC, 21));
+        //Create Buttons
+        buttonOne = new JButton("1 - Make a Booking");
+        buttonOne.setPreferredSize(new Dimension(250, 70));
+        buttonOne.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonOne.setBorder(new LineBorder(Color.BLACK, 5));
         
+        buttonTwo = new JButton("2 - FAQ Section");
+        buttonTwo.setPreferredSize(new Dimension(250, 70));
+        buttonTwo.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonTwo.setBorder(new LineBorder(Color.BLACK, 5));
+        
+        buttonThree = new JButton("3 - About Us");
+        buttonThree.setPreferredSize(new Dimension(250, 70));
+        buttonThree.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonThree.setBorder(new LineBorder(Color.BLACK, 5));
+        
+        buttonFour = new JButton("4 - Admin");
+        buttonFour.setPreferredSize(new Dimension(250, 70));
+        buttonFour.setFont(new Font("Arial", Font.BOLD, 20));
+        buttonFour.setBorder(new LineBorder(Color.BLACK, 5));
+        
+        //Add content to Panels and display on Frame
         topPanel.add(welcomeLabel);
-        topPanel.add(Box.createVerticalStrut(50));
         topPanel.add(subWelcomeLabel);
-        this.add(topPanel, BorderLayout.NORTH);
+        
+        middlePanel.add(buttonOne);
+        middlePanel.add(buttonTwo);
+        middlePanel.add(buttonThree);
+        middlePanel.add(buttonFour);
+        
+        welcomeFrame.add(topPanel, BorderLayout.NORTH);
+        welcomeFrame.add(middlePanel, BorderLayout.CENTER);
+        welcomeFrame.setVisible(true);      
     }
     
     public static void main(String[] args) 
     {
-        JFrame frame = new WelcomeAppGui();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(700, 600);
-        frame.setLocation(700, 300);
-        frame.setVisible(true);
+       new WelcomeAppGui();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
