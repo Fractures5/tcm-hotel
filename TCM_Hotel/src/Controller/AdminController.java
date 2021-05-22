2aaaf132cccf8f4dd83b95ad0003f9a2d9755162
Anuk Silva <vwv9667@autuni.ac.nz>
2021-05-22 13:16:08 +1200

    Added Back the Admin Files

---
+++ TCM_Hotel/src/Controller/AdminController.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.AdminModel;
+import View.AdminView;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import hotelDB.DBManager;
+import hotelDB.HotelProductDB;
+
+/**
+ *
+ * @author Anuk
+ */
+public class AdminController {
+
+    AdminModel modelAdmin;
+    AdminView viewAdmin;
+
+    public AdminController(AdminModel modelAdmin, AdminView viewAdmin) {
+        this.modelAdmin = modelAdmin;
+        this.viewAdmin = viewAdmin;
+
+        /*viewAdmin.getProceedToLogin().addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                handleProceedToLoginClick();
+            }
+        });*/
+
+ /*viewAdmin.getProceedToLogin().addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                handleProceedToLoginClick();
+            }
+        });
+        
+        viewAdmin.getResetAdminDetails().addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                handleResetAdminDetailsClick();
+            }
+        });*/
+
+ /*viewAdmin.getProceedToLogin().addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                mouseEnteredProceedToLogin();
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                mouseExitedProceedToLogin();
+            }
+        });*/
+    }
+
+    public void handleProceedToLoginClick() {
+        System.out.println("ok");
+    }
+
+    public void handleResetAdminDetailsClick() {
+
+    }
+
+    public void mouseEnteredProceedToLogin() {
+
+    }
+
+    public void mouseExitedProceedToLogin() {
+
+    }
+
+}
