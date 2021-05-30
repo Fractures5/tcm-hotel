399c587896021a48bd685e933b084ed1e6e84965
Faaris Ali Khan <tdv2188@autuni.ac.nz>
2021-05-30 14:34:24 +1200

    Added images to About us

--- TCM_Hotel/src/HotelGuiView/AboutUsGuiView.java
+++ TCM_Hotel/src/HotelGuiView/AboutUsGuiView.java
@@ -9,8 +9,14 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.swing.Box;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -31,9 +37,17 @@ public class AboutUsGuiView extends JFrame
     
     private JLabel aboutUsLabel;
     
+    private JLabel imageLabel1; 
+    private JLabel imageLabel2; 
+    private JLabel imageLabel3; 
+    
+    private BufferedImage pic1;
+    private BufferedImage pic2;
+    private BufferedImage pic3;
+    
     private JButton returnButton;
     
-    public AboutUsGuiView ()
+    public AboutUsGuiView () throws IOException
     {
         Toolkit kit = Toolkit.getDefaultToolkit();
         Dimension screenSize = kit.getScreenSize();
@@ -65,13 +79,35 @@ public class AboutUsGuiView extends JFrame
         returnButton.setFont(new Font("Arial", Font.BOLD, 15));
         returnButton.setBounds(300, 400, 855, 400);
         
+        middlePanel = new JPanel();
+        
+        pic1 = ImageIO.read(new File("test/1.jpg"));
+        Image scaledImage1 = pic1.getScaledInstance(100, 100, 700);
+        imageLabel1 = new JLabel(new ImageIcon(scaledImage1));
+        imageLabel1.setBounds(1000, 250, 200, 100);
+        
+        pic2 = ImageIO.read(new File("test/2.png"));
+        Image scaledImage2 = pic2.getScaledInstance(100, 100, 700);
+        imageLabel2 = new JLabel(new ImageIcon(scaledImage2));
+        imageLabel2.setBounds(150, 25, 50, 50);
+        
+        pic3 = ImageIO.read(new File("test/3.jpg"));
+        Image scaledImage3 = pic3.getScaledInstance(100, 100, 700);
+        imageLabel3 = new JLabel(new ImageIcon(scaledImage3));
+        imageLabel3.setBounds(200, 25, 50, 50); 
+             
         bottomPanel = new JPanel();
         bottomPanel.add(Box.createVerticalStrut(100));
         bottomPanel.setBackground(Color.blue);
         
         topPanel.add(aboutUsLabel);
+        middlePanel.add(imageLabel2);
+        middlePanel.add(imageLabel1);
+        middlePanel.add(imageLabel3);
         bottomPanel.add(returnButton);
+        
         aboutUsFrame.add(topPanel, BorderLayout.NORTH);
+        aboutUsFrame.add(middlePanel, BorderLayout.CENTER);
         aboutUsFrame.add(bottomPanel, BorderLayout.SOUTH);
         aboutUsFrame.setVisible(true);
     }
