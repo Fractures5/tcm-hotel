e6b7c814ad69c54f3ba0faeab30f9802997ebea5
Faaris Ali Khan <tdv2188@autuni.ac.nz>
2021-06-01 20:45:30 +1200

    Moving files as proof of MVC working

--- TCM_Hotel/src/tcm/hotel/TCMHotelApp.java
+++ TCM_Hotel/src/tcm/hotel/TCMHotelApp.java
@@ -5,7 +5,7 @@ import HotelGuiView.WelcomeAppGuiMenu;
 
 /**
  * This is the driver class from where the program is ran from, 
- * which includes the main method
+ * which includes the main method and objects of the welcome class.
  * @author faaris
  */
 
